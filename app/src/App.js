579f9f66fb86a98d172f5da0fb8630075aeeb137
ihassa01 <irtaza.hassan@dentsuaegis.com>
2023-01-08 17:43:27 +0000

    Changed from typescript to javascript.

---
+++ app/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+  return (
+    <div className="App">
+      <form>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          type="file"
+          value={selectedFile}
+          onChange={(e) => setSelectedFile(e.target.files[0])}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default App;
